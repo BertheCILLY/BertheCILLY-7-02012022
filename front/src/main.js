4aa28583f369f1f72527a7c3188f803b6b568941
cilly <roses.pourpres@outlook.fr>
2022-02-10 12:54:58 +0100

    suppression du front

--- front/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')//c'est sur cette scop que va être monté tout notre projet 
-
