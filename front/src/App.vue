65f38cd70c3cd2525ec3ed818907ccba7f4b1e63
cilly <roses.pourpres@outlook.fr>
2022-02-10 09:05:47 +0100

    le début

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header id="nav">
+      <router-link to="/">Accueil |</router-link>
+      <router-link to="/about"> A propos |</router-link>
+      <router-link to="/login"> Se connecter |</router-link>
+      <router-link to="/profil"> Mon Profil <v-icon class="icon" name="regular/user"/></router-link>
+    </header>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default{
+    name: 'Main',
+      data () {
+        return {
+          groupName : 'Groupomania',
+        }
+      },
+    }
 </script>
 
-<style>
+
+<style scoped lang="scss">
+$primary: #283A59 !default;
+
+body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100vh;
+    position: relative;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 2em;
+  background-color: $primary;
+
+  a {
+    color: #ffffff;
+
+    &.router-link-exact-active {
+      color: #ffffff;
+    }
+  }
+  
+  .icon{
+    color:#ffffff;
+  }
 }
 </style>
